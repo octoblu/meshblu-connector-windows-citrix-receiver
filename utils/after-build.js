424072516b3830df18d199de4ebb9edc4f982f0c
Roy van de Water <r@octoblu.com>
2017-07-19 16:05:59 -0700

    v1.0.7 Try again

--- utils/after-build.js
+++ utils/after-build.js
@@ -1,27 +1,27 @@
-const path = require("path")
-const fs = require("fs-extra")
+const fs = require('fs-extra')
+const path = require('path')
 
 function die(error) {
-  console.error(error.stack)
+  console.error(error.stack) // eslint-disable-line no-console
   process.exit(1)
 }
 
 function copy(source, dest, callback) {
-  fs.copy(source, dest, error => {
+  fs.copy(source, dest, (error) => {
     if (error) return die(error)
     callback()
   })
 }
 
 function copyAssets(callback) {
-  const source = path.resolve("./assets")
+  const source = path.resolve('./assets')
   const dest = path.resolve(`./deploy/${process.env.MESHBLU_CONNECTOR_TARGET}/bin/assets`)
 
   copy(source, dest, callback)
 }
 
 function copyEdgeCS(callback) {
-  const source = path.resolve("./node_modules/edge-cs/lib/edge-cs.dll")
+  const source = path.resolve('./node_modules/edge-cs/lib/edge-cs.dll')
   const dest = path.resolve(`./deploy/${process.env.MESHBLU_CONNECTOR_TARGET}/bin/edge-cs.dll`)
   copy(source, dest, callback)
 }
--- utils/build.js
+++ utils/build.js
@@ -1,33 +1,49 @@
-const fs = require('fs-extra')
+/* eslint-disable no-console */
 const path = require('path')
+const fs = require('fs-extra')
+const { exec } = require('child_process')
 
 function die(error) {
-  console.error(error.stack) // eslint-disable-line no-console
+  console.error(error.stack)
   process.exit(1)
 }
 
-function copy(source, dest, callback) {
-  fs.copy(source, dest, (error) => {
+function cleanup(callback) {
+  fs.remove('node_modules/edge/build', callback)
+}
+
+function run(cmd, callback) {
+  const options = {
+    cwd: path.resolve('node_modules/edge'),
+    env: process.env,
+  }
+
+  exec(cmd, options, (error, stdout, stderr) => {
+    if (error) return die(error)
+    console.log(stdout)
+    console.error(stderr)
+    callback()
+  })
+}
+
+function rid(callback) {
+  const options = {
+    cwd: path.join(process.cwd(), 'utils'),
+  }
+  exec('rid.exe ..\\node_modules\\edge\\build\\Release\\edge_nativeclr.node node.exe meshblu-connector-skype.exe', options, (error, stdout, stderr) => {
+    console.log({ stdout, stderr })
     if (error) return die(error)
     callback()
   })
 }
 
-function copyAssets(callback) {
-  const source = path.resolve('./assets')
-  const dest = path.resolve(`./deploy/${process.env.MESHBLU_CONNECTOR_TARGET}/bin/assets`)
-
-  copy(source, dest, callback)
-}
-
-function copyEdgeCS(callback) {
-  const source = path.resolve('./node_modules/edge-cs/lib/edge-cs.dll')
-  const dest = path.resolve(`./deploy/${process.env.MESHBLU_CONNECTOR_TARGET}/bin/edge-cs.dll`)
-  copy(source, dest, callback)
-}
-
-copyAssets(() => {
-  copyEdgeCS(() => {
-    process.exit(0)
+cleanup(() => {
+  run('node-gyp configure', () => {
+    run('node-gyp build', () => {
+      rid(() => {
+        console.log('all done!')
+        process.exit(0)
+      })
+    })
   })
 })
