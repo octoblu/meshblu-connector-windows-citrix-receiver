4a55814e7b7cabe54d94d43898caf58f840901c6
Roy van de Water <r@octoblu.com>
2017-07-20 15:02:22 -0700

    v1.1.12 :-(

--- assets/cs/checkXenDesktopConnection.cs
+++ assets/cs/checkXenDesktopConnection.cs
@@ -4,10 +4,17 @@ using WFICALib;
 
 public class Startup {
   public async Task<object> Invoke(dynamic ignored) {
+    System.Console.WriteLine("Invoke")
     ICAClientClass ica = new ICAClientClass();
+    System.Console.WriteLine("new ICAClientClass");
     var sessionsHandle = ica.EnumerateCCMSessions();
+    System.Console.WriteLine("EnumerateCCMSessions");
     var sessionsCount  = ica.GetEnumNameCount(sessionsHandle);
+    System.Console.WriteLine("GetEnumNameCount");
     ica.CloseEnumHandle(sessionsHandle);
+    System.Console.WriteLine("CloseEnumHandle");
+    System.Console.WriteLine("sessionsCount");
+    System.Console.WriteLine(sessionsCount);
     return (sessionsCount > 0);
   }
 }
