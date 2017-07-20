adf39c393124e7bebe111f4626470411a2e9153a
Roy van de Water <r@octoblu.com>
2017-07-20 11:16:09 -0700

    v1.1.4 Use octodash

--- command.js
+++ command.js
@@ -1,17 +1,58 @@
 #!/usr/bin/env node
 
-const child_process = require('child_process') // eslint-disable-line camelcase
-const MeshbluConfig = require('meshblu-config')
-const MeshbluFirehose = require('meshblu-firehose-socket.io')
 const MeshbluHttp = require('meshblu-http')
+const OctoDash = require('octodash')
+const path = require('path')
 const Connector = require('./lib/Connector')
 
+const CLI_OPTIONS = [
+  {
+    names: ['uuid', 'u'],
+    type: 'string',
+    env: 'MESHBLU_UUID',
+    required: true,
+    help: 'Meshblu UUID',
+    helpArg: 'UUID',
+  },
+  {
+    names: ['token', 't'],
+    type: 'string',
+    env: 'MESHBLU_TOKEN',
+    required: true,
+    help: 'Meshblu Token',
+    helpArg: 'TOKEN',
+  },
+  {
+    names: ['domain', 'd'],
+    type: 'string',
+    default: 'octoblu.com',
+    env: 'MESHBLU_DOMAIN',
+    help: 'Meshblu SRV Domain',
+    helpArg: 'DOMAIN',
+  },
+  {
+    names: ['env-file'],
+    type: 'string',
+    default: path.join(process.cwd(), '.env'),
+    env: 'MESHBLU_ENV_FILE',
+    help: 'dotenv file',
+    helpArg: 'FILE',
+    completionType: 'file',
+  },
+]
+
 class Command {
-  constructor() {
-    const meshbluConfig = new MeshbluConfig()
-    const meshbluFirehose = new MeshbluFirehose({ meshbluConfig: meshbluConfig.toJSON() })
-    const meshbluHttp = new MeshbluHttp(meshbluConfig.toJSON())
-    this.connector = new Connector({ child_process, meshbluHttp, meshbluFirehose })
+  constructor({ argv }) {
+    this.octoDash = new OctoDash({
+      argv,
+      cliOptions: CLI_OPTIONS,
+      name: this.connectorPackageJSON.name,
+      version: this.connectorPackageJSON.version,
+    })
+
+    const { uuid, token, domain } = this.octoDash.parseOptions()
+    const meshbluHttp = new MeshbluHttp({ uuid, token, domain, resolveSrv: true })
+    this.connector = new Connector({ meshbluHttp })
   }
 
   static panic(error) {
