aaf4e780e4fa8b44f545b16f3046c3c7de89ad34
amirmocto <amir.mukeri@citrix.com>
2017-08-28 13:43:32 -0700

    WFICAlib broken with August 2017 release now using System.Management.dll to check if Xendesktop is running

--- assets/cs/checkXenDesktopConnection.cs
+++ assets/cs/checkXenDesktopConnection.cs
@@ -1,13 +1,12 @@
 using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
-using WFICALib;
+using System.Management;
 
 public class Startup {
-  public async Task<object> Invoke(dynamic ignored) {
-    ICAClientClass ica = new ICAClientClass();
-    var sessionsHandle = ica.EnumerateCCMSessions();
-    var sessionsCount  = ica.GetEnumNameCount(sessionsHandle);
-    ica.CloseEnumHandle(sessionsHandle);
-    return sessionsCount;
+  public async Task<object> Invoke(dynamic ignored){
+    Process[] processlist = Process.GetProcessesByName("wfica32");
+    if(processlist == null || processlist.Length == 0) return 0;
+    return processlist.Length;
   }
 }
